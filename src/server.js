f811b59ed4eb8ed3e7fb86fe808db424e213b998
WooMin <hanulbom@gmail.com>
2020-04-06 17:24:30 +0900

    #2.3 Chat with SocketIO

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
-app.unsubscribe(logger("dev"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
 
@@ -18,8 +18,14 @@ const server = app.listen(PORT, handleListening);
 
 const io = socketIO.listen(server);
 
-let sockets = [];
-
 io.on("connection", (socket) => {
-  sockets.push(socket.id);
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Anon",
+    });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
 });
